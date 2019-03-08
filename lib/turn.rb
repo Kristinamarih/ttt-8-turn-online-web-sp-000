300d2b851bf04839717be8a3811d606280358153
Kristina Mari Horensky <kristinahorensky@gmail.com>
2019-03-08 05:32:37 +0000

    Automatically backed up by Learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -36,9 +36,8 @@ def turn(board)
   index = input_to_index(user_input)
   if valid_move?(board, index)
     move(board, index)
-    turn(board)
   else
-    board(turn)
+    turn(board)
   end
 end
   
